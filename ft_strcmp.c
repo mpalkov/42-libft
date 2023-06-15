241aca0a81d748b2d46d55cdca8d2eb4597a5e53
Martin Palkov <mpalkov@student.42barcelona.com>
2023-06-15 15:09:03 +0200

    added strcmp

---
+++ ft_strcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpalkov <mpalkov@student.42barcelo>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 13:44:34 by mpalkov           #+#    #+#             */
+/*   Updated: 2023/06/13 17:04:35 by mpalkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+			++i;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
